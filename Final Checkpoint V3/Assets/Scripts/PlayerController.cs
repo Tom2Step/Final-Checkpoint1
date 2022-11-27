70b5129ed4f05a9deb6c924bc96d4fe9963f5c65
Tom2Step <118644553+Tom2Step@users.noreply.github.com>
2022-11-27 15:57:01 -0700

    Update PlayerController.cs

--- Final Checkpoint V3/Assets/Scripts/PlayerController.cs
+++ Final Checkpoint V3/Assets/Scripts/PlayerController.cs
@@ -5,71 +5,29 @@ using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-
-    [SerializeField] float speed;
-    [SerializeField] float rpm;
-    [SerializeField] private float horsePower = 0;
-    [SerializeField] TextMeshProUGUI speedometerText;
-    [SerializeField] TextMeshProUGUI rpmText;
-    public float turnSpeed;
-    public float horizontalInput;
-    public float forwardInput;
-    private Rigidbody playerRB;
-    [SerializeField] GameObject centerOfMass;
-    [SerializeField] List<WheelCollider> allWheels;
-    [SerializeField] int wheelsOnGround;
+    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float turnSpeed;
+    [SerializeField] private float horizontalInput;
+    [SerializeField] private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerRB = GetComponent<Rigidbody>();
-        playerRB.centerOfMass = centerOfMass.transform.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (IsOnGround())
-        {
-            //This is where we get player input
-            forwardInput = Input.GetAxis("Vertical");
-            horizontalInput = Input.GetAxis("Horizontal");
 
-            // We move the vehicle forward
-            //transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-            playerRB.AddRelativeForce(Vector3.forward * horsePower * forwardInput);
+        //This is where we get player input
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
 
-            //We move turn the vehicle 
-            transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
-        }
-        
+        // We move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
 
-        //Calculate speed
-        speed = Mathf.RoundToInt(playerRB.velocity.magnitude * 2.237f);
-        speedometerText.SetText("Speed: " + speed + " MPH");
-
-        //Display RPM
-        rpm = Mathf.Round((speed % 30) * 40);
-        rpmText.SetText("RPM: " + rpm);
-
-    }
-    bool IsOnGround()
-    {
-        wheelsOnGround = 0;
-        foreach (WheelCollider wheel in allWheels)
-        {
-            if (wheel.isGrounded)
-            {
-                wheelsOnGround++;
-            }
-        }
-        if (wheelsOnGround >= 2)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        //We move turn the vehicle 
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
